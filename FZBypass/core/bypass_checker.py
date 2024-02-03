288f0430ef1059f88d9a1bff3c99f248a2bf2093
Midhun <103011794+SPARKBRO@users.noreply.github.com>
2024-02-03 14:47:54 +0530

    Update bypass_checker.py

--- FZBypass/core/bypass_checker.py
+++ FZBypass/core/bypass_checker.py
@@ -40,7 +40,7 @@ async def direct_link_checker(link, onlylink=False):
         blink = await transcript(link, "https://go.ronylink.com/", "https://livejankari.com/", 9)
    #elif bool(match(r"https?:\/\/(gyanilinks|gtlinks)\.\S+", link)):
        #blink = await gyanilinks(link)
-  elif bool(match(r"https?:\/\/gtlinks\.\S+", link)):
+    elif bool(match(r"https?:\/\/gtlinks\.\S+", link)):
         blink = await transcript(link, "https://golink.gyanitheme.com/", "https://www.hipsonyc.com/", 7)
     elif bool(match(r"https?:\/\/.+\.tnshort\.\S+", link)):
         blink = await transcript(link, "https://go.tnshort.net/", "https://market.finclub.in/", 8)
