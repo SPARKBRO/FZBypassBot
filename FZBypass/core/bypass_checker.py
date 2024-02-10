e2f64ab0b53f78f0ad3237b77b9055b5fc522d09
Midhun <103011794+SPARKBRO@users.noreply.github.com>
2024-02-10 07:22:58 +0530

    Update bypass_checker.py

--- FZBypass/core/bypass_checker.py
+++ FZBypass/core/bypass_checker.py
@@ -38,12 +38,12 @@ async def direct_link_checker(link, onlylink=False):
         blink = await try2link(link)
     elif bool(match(r"https?:\/\/ronylink\.\S+", link)):
         blink = await transcript(link, "https://go.ronylink.com/", "https://livejankari.com/", 9)
-   #elif bool(match(r"https?:\/\/(gyanilinks|gtlinks)\.\S+", link)):
-       #blink = await gyanilinks(link)
+    elif bool(match(r"https?:\/\/(gyanilinks|gtlinks)\.\S+", link)):
+        blink = await gyanilinks(link)
     elif bool(match(r"https?:\/\/shrinkme\.\S+", link)):
         blink = await transcript(link, "https://themezon.net/", "https://en.shrinke.me/", 10)
-    elif bool(match(r"https?:\/\/gtlinks\.\S+", link)):
-        blink = await transcript(link, "https://gyanitheme.com/", "https://www.hipsonyc.com/", 7)
+    #elif bool(match(r"https?:\/\/gtlinks\.\S+", link)):
+        #blink = await transcript(link, "https://gyanitheme.com/", "https://www.hipsonyc.com/", 7)
     elif bool(match(r"https?:\/\/.+\.tnshort\.\S+", link)):
         blink = await transcript(link, "https://go.tnshort.net/", "https://jrlinks.in/", 8)
     elif bool(match(r"https?:\/\/(xpshort|push.bdnewsx|techymozo)\.\S+", link)):
